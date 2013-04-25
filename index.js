db5d366cd4ac93ce0e1642f84f3e26220baa9065
Harrison Massey <harrison.massey@gmail.com>
2013-04-25 11:30:04 -0500

    Added blueprint method with help of @maxmechanic

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-module.exports = function(speed, bearing){
+module.exports = windsock
+
+function windsock() {}
+
+windsock.prototype.generate = function(speed, bearing){
   bearing = Math.round(bearing/45)*45
   var rads = bearing/360*2*Math.PI
   return function(x,y,z) {
     if (
-        (x === 0 && z === 0 && y > 0 && y <= 10) ||
-        (
-          y === 10
-          && x/z === Math.round(Math.cos(rads) / Math.sin(rads))
-          && Math.sqrt(Math.pow(x,2) + Math.pow(z,2)) <= speed
-          && (rads > Math.PI ? z < 0 : z >= 0)
-        )
+      (x === 0 && z === 0 && y > 0 && y <= 10) ||
+      (
+        y === 10
+        && x/z === Math.round(Math.cos(rads) / Math.sin(rads))
+        && Math.sqrt(Math.pow(x,2) + Math.pow(z,2)) <= speed
+        && (rads > Math.PI ? z < 0 : z >= 0)
+      )
     ) return 1
 
     return 0
   }
+}
+
+windsock.prototype.blueprint = function(pos, speed, bearing) {
+  bearing = Math.round(bearing/45)*45
+  var rads = bearing/360*2*Math.PI
+  var sockHeight = pos[1] + 10
+  var windsockCoordinates = []
+
+  for (var i = 0; i <= 10; i++) { windsockCoordinates.push([pos[0], i + pos[1], pos[2]]) } //build pole relative to position
+
+  for (var j = -1 * speed; j < speed; j++ ) {
+    for (var k = -1 * speed; k < speed; k++) {
+      if (
+        j/k === Math.round(Math.cos(rads) / Math.sin(rads) )
+        && Math.sqrt(Math.pow(j,2) + Math.pow(k,2)) <= speed
+        && (rads > Math.PI ? k < 0 : k >= 0)
+      )
+        windsockCoordinates.push([j + pos[0], sockHeight, k + pos[2]])
+    }
+  }
+  return windsockCoordinates
 }
--- test.js
+++ test.js
@@ -1,20 +1,23 @@
 var createGame = require('voxel-engine')
 var player = require('voxel-player')
 var texturePath = require('painterly-textures')(__dirname)
-var config = require('./config.js')
-var windsock = require('./')(10,86)
-
+var Windsock = require('./')
+var windsock = new Windsock()
 var game = createGame({
   texturePath: texturePath,
   generate: function(x, y, z) {
     if(y === 0) return 1
-    return windsock(x,y,z)
+    return windsock.generate(10,86)(x,y,z)
   }
 })
 var container = document.body
 game.appendTo(container)
 
-//Here's what you're wanting
 var avatar = player(game)()
 avatar.possess()
 avatar.yaw.position.set(0,5,5)
+
+var newsock = windsock.blueprint([20,0,20], 15, 43)
+newsock.forEach(function(pos) {
+    game.createBlock(pos,3)
+})
